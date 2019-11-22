b5d4ea6a6f979e13237282ce73d91775d8294fa6
cmattey <cmattey@dons.usfca.edu>
2019-11-21 18:43:37 -0800

    Added example to scale down image for faster processing

--- project_folder/seam_carver.py
+++ project_folder/seam_carver.py
@@ -15,6 +15,27 @@ def image_display_example(img_path):
     if keyPress == 27:
         cv2.destroyAllWindows()
 
+
+def image_scale_down_example(img_path):
+    """
+    Example to scale down image to process faster
+    """
+
+    img = cv2.imread(img_path, 1)
+
+    scale_factor = 0.1
+    new_height = int(len(img)*scale_factor)
+    new_width = int(len(img[0])*scale_factor)
+
+    img = cv2.resize(img, (new_height, new_width))
+
+    cv2.imshow('scaled_down', img)
+    cv2.waitKey(0)
+    if keyPress == 27:
+        cv2.destroyAllWindows()
+
+image_scale_down_example('example.jpg')
+
 def image_transform_example(img_path):
     """
     Example function to transform into image using an energy function.
@@ -27,6 +48,5 @@ def image_transform_example(img_path):
     func = delta_x + delta_y
     """
 
-    
 
-image_display_example('example.jpg')
+# image_transform_example('example.jpg')
